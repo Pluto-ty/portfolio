3625bc3b94f3f9895cc786b9ba935b283b1d9c21
Pluto <thyagohyung@gmail.com>
2021-06-13 23:16:51 -0300

    Finalizando Mobile

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const toggle = {
-   switch() {
-      let $html = document.querySelector("html");
-      $html.classList.toggle("dark-mode");
-   },
-};
+document.querySelector("#js-home").addEventListener("click", (event) => {
+   if (event.deltaY > 0) {
+      event.target.scrollBy(500, 0);
+   } else {
+      event.target.scrollBy(-300, 0);
+   }
+});
