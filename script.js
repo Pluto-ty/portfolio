76b6c40887b0a6e3605251c61a5c66fb4b492848
Pluto <thyagohyung@gmail.com>
2021-06-15 23:49:32 -0300

    Faltando apenas a animação das navegações e adaptação para o mobile

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-document.querySelector("#js-home").addEventListener("click", (event) => {
+document.querySelector("#js-home").addEventListener("wheel", (event) => {
+   // alert("test");
+   let pages = document.querySelector("#js-pages-container");
    if (event.deltaY > 0) {
-      event.target.scrollBy(500, 0);
+      pages.scrollBy(0, 700);
    } else {
-      event.target.scrollBy(-300, 0);
+      pages.scrollBy(0, -700);
    }
 });
+
+document
+   .querySelector("#js-projects-nav")
+   .addEventListener("wheel", (event) => {
+      let projects = document.querySelector("#js-projects-container");
+      if (event.deltaY > 0) {
+         projects.scrollBy(0, 700);
+      } else {
+         projects.scrollBy(0, -700);
+      }
+   });
+
+function showText() {
+   document.querySelector("#js-arrow").style.display = "none";
+   setTimeout(() => {
+      document.querySelector("#js-all-projects-text").style.display = "block";
+   }, 150);
+}
+
+function removeText() {
+   setTimeout(() => {
+      document.querySelector("#js-arrow").style.display = "block";
+      document.querySelector("#js-all-projects-text").style.display = "none";
+   }, 150);
+}
